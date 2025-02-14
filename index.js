d77df22e19370d85e8a13e8807379533c81c3bba
SrYonkai <kevinjogos608@gmail.com>
2025-02-14 19:46:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Rede Lunar", "🎮 Jogando naruto rp"];
+const statusMessages = ["🎧 Olympus Network", "🎮 Trabalhando em Olympus 2"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
